4ea82eb2baaffbca2e4d746d5f3e636e16942e51
Arkady Rost <1239844+arkrost@users.noreply.github.com>
2024-09-28 23:14:28 +0100

    Custom adjustments

--- after/queries/kotlin/highlights.scm
+++ after/queries/kotlin/highlights.scm
@@ -6,4 +6,16 @@
 (class_declaration
   (type_identifier) @AlabasterDefinition)
 
+(object_declaration
+  (type_identifier) @AlabasterDefinition)
+
+;(variable_declaration
+;  (simple_identifier) @AlabasterDefinition)
+
+(secondary_constructor
+  ("constructor") @AlabasterDefinition)
+
+(companion_object
+  ("companion") @AlabasterDefinition)
+
 (modifiers (annotation (user_type (type_identifier) @AlabasterPunctuation)))
